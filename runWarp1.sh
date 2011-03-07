1a6c7dc494498041635c2de0322fbe70ae1ea0d6
Ryan Eckbo <reckbo@pnl-t55-9.bwh.harvard.edu>
2011-03-07 14:44:33 -0500

    Renamed example scripts

--- runWarp1.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/csh
-
-src/WarpVolume --inputVolume /projects/schiz/3Tdata/case01009/diff/01009-dwi-filt-Ed.nhdr --warp /spl_unsupported/pnlfs/reckbo/data/10_masked_cases_affined/G119_128_0_deformation.nii.gz --resultsDirectory ./
