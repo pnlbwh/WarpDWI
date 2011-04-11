53a4808f6f0944f95b0001aea808b559b42322cd
Ryan <reckbo@bwh.harvard.edu>
2011-04-11 16:11:30 -0400

    Committing the test shell scripts

---
+++ test_warp_dwi.sh
@@ -0,0 +1,2 @@
+#!/bin/csh
+src/WarpVolume /spl_unsupported/pnlfs/reckbo/projects/CreateDWIAtlas/tests/input/01019-Rgd-norm.nhdr --warp /spl_unsupported/pnlfs/reckbo/data/10_masked_cases_affined/G119_128_0_deformation.nii.gz warped_DWI_volume.nrrd
